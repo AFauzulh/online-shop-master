fba96d84c8fbd38f598aadd36871694886a8dd1f
AFauzulh <alfirsaafauzulh@gmail.com>
2020-03-17 22:58:57 +0700

    adding basic validation with express validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const {
+    check
+} = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -16,7 +19,13 @@ router.get('/reset/:token', authController.getNewPassword);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+    '/signup',
+    check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email'),
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
