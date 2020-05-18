8ff30949616b7a55eabb18020c9d2d7e2d08053e
AFauzulh <alfirsaafauzulh@gmail.com>
2020-05-18 22:20:01 +0700

    adding client side async request for admin delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -185,8 +185,8 @@ exports.getProducts = (req, res, next) => {
         });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
 
     Product.findById(prodId)
         .then(product => {
@@ -201,11 +201,13 @@ exports.postDeleteProduct = (req, res, next) => {
         })
         .then(result => {
             console.log(`Product Deleted`);
-            res.redirect("/admin/products");
+            res.status(200).json({
+                message: 'success!'
+            });
         })
         .catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({
+                message: 'Failed delete product'
+            });
         });
 };
